0cd98946d312184483897ac73d5f1674018a5f01
SSEUNGSSEUNGWOO <jsw7980@naver.com>
2025-02-16 17:53:04 +0900

    modified:   automata/q14.py 	modified:   automata/q16.py

--- automata/q14.py
+++ automata/q14.py
@@ -3,7 +3,7 @@ from datetime import datetime
 print(datetime.today().strftime("%Y-%m-%d"))
 
 # from datetime import datetime 
-#       클래스           클래스 내부 외장함수
+#       모듈            모듈 안의 클래스
 # datetime.today()            # 현재 날짜 가져오기
 # datetime.today().year       # 현재 연도 가져오기
 # datetime.today().month      # 현재 월 가져오기
--- automata/q16.py
+++ automata/q16.py
@@ -1,4 +1,15 @@
-clock, minute = input().split()
+hours, minutes = input().split()
 cook_time = input()
-clock , minute, cook_time = int(clock), int(minute), int(cook_time)
+hours, minutes, cook_time = int(hours), int(minutes), int(cook_time)
 
+total_minutes = (hours * 60) + minutes + cook_time
+hours = int(total_minutes / 60)
+minutes = total_minutes % 60
+
+if hours>=24:
+    print(f"{hours-24} {minutes}")
+else:
+    print(f"{hours} {minutes}")
+
+
+# 정수형 변수를 저장한 변수의 type은 int이다.
